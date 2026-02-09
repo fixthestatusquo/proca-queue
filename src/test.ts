d708b7528f4e835cdc04bf37a75f3bcc377738ac
1v4n4 <ivananl@yahoo.com>
2026-02-09 22:12:14 +0100

    Add simple test

---
+++ src/test.ts
@@ -0,0 +1,41 @@
+import { ActionMessage } from './actionMessage';
+import { syncQueue } from './queue';
+import type { Event } from './events';
+
+const {
+  PROCA_USERNAME,
+  PROCA_PASSWORD,
+  PROCA_QUEUE,
+} = process.env;
+if (!PROCA_QUEUE) {
+  throw new Error('PROCA_QUEUE environment variable is required');
+};
+
+const url = () => {
+  if (!PROCA_USERNAME || !PROCA_PASSWORD) {
+      throw new Error(
+        'Either PROCA_URL or both PROCA_USERNAME and PROCA_PASSWORD must be set'
+      );
+    }
+
+    return `amqps://${PROCA_USERNAME}:${encodeURIComponent(
+      PROCA_PASSWORD
+    )}@api.proca.app/proca_live`;
+};
+
+
+async function handler(msg: ActionMessage | Event) {
+  console.log('msg:', msg);
+  return false; // safe: requeue
+};
+
+
+syncQueue(
+  url(),
+  PROCA_QUEUE!,
+  handler,
+  {
+    concurrency: 1,
+    tag: 'local-dev',
+  }
+);
